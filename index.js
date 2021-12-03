486c85373398dd898a0583822f1bd4e9e53a5df9
Aldrin Sosa <sosaadrin2006@gmail.com>
2021-12-03 01:47:16 -0500

    Modifique el apartado de skills, agrege la seccion de projects y le di funcionalidad al navbar

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const navToggle = document.querySelector(".nav-toggle-button");
 const navMenu = document.querySelector(".nav-menu");
+const navAbout = document.querySelector(".navAbout");
+const navSkills = document.querySelector(".navSkills");
+const navProjects = document.querySelector(".navProjects");
 
 navToggle.addEventListener("click", () => {
   navMenu.classList.toggle("nav-menu_visible");
 });
+
+navAbout.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+}); 
+
+navSkills.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+}); 
+
+navProjects.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-menu_visible");
+}); 
